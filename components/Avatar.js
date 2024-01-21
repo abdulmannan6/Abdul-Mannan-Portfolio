8c7f386a50a86b02ce51132144a62b036dcb352b
Abdul <itsmannan56@gmail.com>
2024-01-21 14:51:13 +0500

    Initial commit

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,7 +5,7 @@ const Avatar = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
       <Image
-        src={'/../public/mannan-avatar.png'}
+        src={'/mannan-avatar.png'}
         width={737}
         height={678}
         alt={'avatar'}
