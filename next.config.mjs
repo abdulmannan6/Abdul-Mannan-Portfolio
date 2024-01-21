ff0a3c4ed03dcc41eb6deac12100e621eb57b938
Abdul <itsmannan56@gmail.com>
2024-01-21 13:41:59 +0500

    NextJs Config File

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: 'https://github.com/abdulmannan6/abdul-mannan-portfolio.git'
+};
 
 export default nextConfig;
