950ced86787e89752d2115c2f795953b81507c0e
Abdul Mannan <133950133+abdulmannan6@users.noreply.github.com>
2024-03-12 12:52:19 +0500

    Update Transition.js

--- components/Transition.js
+++ components/Transition.js
@@ -19,11 +19,11 @@ const transitionVariatns = {
 const Transition = () => {
   return (
     <>
-      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]' variants={transitionVariatns} initial='initial' animate='animate' exit='exit' transition={{ delay: 0.2, duration: 0.6, ease: 'easeInOut' }}>
+      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]' variants={transitionVariatns} initial='initial' animate='animate' exit='exit' transition={{ delay: 0.2, duration: 0.2, ease: 'easeInOut' }}>
       </motion.div>
-      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]' variants={transitionVariatns} initial='initial' animate='animate' exit='exit' transition={{ delay: 0.4, duration: 0.6, ease: 'easeInOut' }}>
+      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]' variants={transitionVariatns} initial='initial' animate='animate' exit='exit' transition={{ delay: 0.3, duration: 0.3, ease: 'easeInOut' }}>
       </motion.div>
-      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]' variants={transitionVariatns} initial='initial' animate='animate' exit='exit' transition={{ delay: 0.6, duration: 0.6, ease: 'easeInOut' }}>
+      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]' variants={transitionVariatns} initial='initial' animate='animate' exit='exit' transition={{ delay: 0.4, duration: 0.3, ease: 'easeInOut' }}>
       </motion.div>
     </>
   );
